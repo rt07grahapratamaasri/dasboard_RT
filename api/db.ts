5abd28af6e80eb985d6d6367a521dee4f5055cdf
Aziz K. <operasionalitcito@gmail.com>
2026-07-06 10:46:22 +0700

    Fix database connection

--- api/db.ts
+++ api/db.ts
@@ -1,5 +1,8 @@
-import { Pool } from '@neondatabase/serverless';
+import { Pool } from 'pg';
 
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL
+  connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
